72bd826510162999e107ae1cdfc0da606a105782
d <lizard43@gmail.com>
2017-11-26 18:51:21 -0500

    initial

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # library.chrome.extension
-Chrome extension for searching library for selected words 
+Chrome extension for searching library for highlighted/selected words
+
+* Clone/download repo
+* Load via settings menu, More Tools -> Extensions. Then "Load Unpacked Extension"
+* Highlight word(s) in webpage, right-click and select menu item "Search Library for ..."
+
+- My library has separate websites for books and ebooks so I open separate Chrome tabs for each. Remove the eBook URL and chrome.tabs.create lines if your library doesn't have separate sites.
---
+++ content.js
@@ -0,0 +1,32 @@
+
+// A generic onclick callback function.
+function onClick(info, tab) {
+
+    console.log("info: " + JSON.stringify(info));
+    console.log("tab: " + JSON.stringify(tab));
+    
+    if (info && info.selectionText) {
+
+        var words = info.selectionText;
+
+        // update the two URLs with your library's links 
+        var bookUrl = "http://alpl.ent.sirsi.net/client/en_US/default/search/results/?q=" + words;
+        chrome.tabs.create({ url: bookUrl});
+        
+        // my library has separate websites for books and ebooks.
+        // remove these lines if your library doesn't
+        var eBookUrl = "https://acpl.overdrive.com/search?query=" + words;
+        chrome.tabs.create({ url: eBookUrl});        
+    }
+  }
+  
+  // Create one test item for each context type.
+//   var contexts = ["page","selection","link","editable","image","video","audio"];
+  var contexts = ["selection"];
+  for (var i = 0; i < contexts.length; i++) {
+    var context = contexts[i];
+
+    var title = "Search Library for \"%s\"";
+    var id = chrome.contextMenus.create({"title": title, "contexts":[context],
+                                         "onclick": onClick});
+  }
